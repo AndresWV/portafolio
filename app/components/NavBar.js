4c60195d4c84e4af57f6ea38c113bcb6858cdfad
AndresWV <awallberg18@alumnos.utalca.cl>
2023-05-06 15:17:48 -0400

    init project with nextjs/nextui

---
+++ app/components/NavBar.js
@@ -0,0 +1,10 @@
+"use client";
+export default function NavBar() {
+    return (
+        <div>
+            <h1>
+                NavBar
+            </h1>
+        </div>
+    );
+}
---
+++ app/page.js
@@ -0,0 +1,12 @@
+"use client";
+import * as React from 'react';
+import { NextUIProvider } from '@nextui-org/react';
+import NavBar from './components/NavBar';
+
+export default function Home() {
+  return (
+    <NextUIProvider>
+      <NavBar />
+    </NextUIProvider>
+  )
+}
